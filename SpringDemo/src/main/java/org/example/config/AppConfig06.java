bbc237488865d30485d4b1ca1be7653ca27da041
wanghaoying <1014977806@qq.com>
2021-05-15 23:18:58 +0800

    add springDemo aop config

---
+++ SpringDemo/src/main/java/org/example/config/AppConfig06.java
@@ -0,0 +1,16 @@
+package org.example.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * AOP: [动态代理]
+ *     指在程序在运行期间可以将某段代码动态的切入到另一个方法的指定位置，不需要
+ * 将这段代码硬编码到对应的方法中就可以完成预期的功能
+ *
+ *      1、
+ */
+@Configuration
+public class AppConfig06 {
+
+}
